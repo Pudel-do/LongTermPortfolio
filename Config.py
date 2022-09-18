bb9e37c96675acf371b56cabc413e84480384901
unknown <matthiaspudel@gmail.com>
2022-09-18 13:58:45 +0200

    Add parameter file and config class for loading parameters

---
+++ Config.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+class Config:
+    def __init__(self, config_name):
+        self.config_name = config_name
+        self.config = self.load()
+        
+    def load(self):
+        with open (self.config_name) as conf:
+            config = json.load(conf)
+        return config
+    
+ParameterConfig = Config("Parameter.json")
+
